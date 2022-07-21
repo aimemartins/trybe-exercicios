eca9f5271515fd57dae4c62a77df6e97fec08ad8
AIMÊ <aimemartins@hotmail.com>
2022-07-21 08:02:03 -0300

    Exercício 4.1.2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-lógica-de-programação/dia-1-primeiros-passos/scripts-02.js
@@ -0,0 +1,12 @@
+// 1 Parte
+
+const base = 5;
+const height = 8;
+// 2 Parte
+const area = base * height 
+console.log("A área do retângulo é:",area);
+// 3 Parte
+const perimeter = (2*base) + (2*height);
+console.log("O perímetro do retângulo é:",perimeter);
+
+
