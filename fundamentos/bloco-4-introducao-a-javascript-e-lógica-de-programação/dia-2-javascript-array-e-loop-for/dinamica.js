91043ada301bba66c387254e2d026dd093a54c63
AIMÊ <aimemartins@hotmail.com>
2022-07-24 23:03:03 -0300

    Exercícios do dia 4.2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-lógica-de-programação/dia-2-javascript-array-e-loop-for/dinamica.js
@@ -0,0 +1,17 @@
+// 1. Faça um algoritmo que calcule a soma de 1 a 50 usando a estrutura "for" e retorne no formato: A soma de 1 a 50 é: X.
+
+//2. Crie um algoritmo que conte quantos números do intervalo entre 2 e 150 são divisíveis por 3. Caso a quantidade seja igual a 50, exiba uma mensagem secreta.
+
+//3. Crie um algoritmo que recebe a idade de Nat, Renan e Sincero e imprime quem é a pessoa mais nova no formato: "Pessoa" é a mais nova.
+
+
+
+
+console.log(sum);
+
+let nunbers = [];
+let div3 = [];
+for (let index = 2 ; index <= 150 ; index ++) {
+div.push(( nunbers [index] % 3 === 0 ))
+}
+
